68ecbbab7e780b392a11e1d93bfdded3fcee8409
lcs-jdai <jdai@lcs.on.ca>
2022-04-27 20:17:11 -0400

    I start making my sunset animation and I did some shadow and rotating effect on it.

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -8,13 +8,87 @@
 import SwiftUI
 
 struct CustomComposableView: View {
+    
+   
+    
+    @State private var yShadow = 20
+    
+    
+  
+//    @State var xOffset = -100.0
+//
+//    @State private var rotation = 0.0
+//
+//
+//    //trigger to start the animation
+    let timer = Timer.publish(every: 1.00, on: .main, in: .common).autoconnect()
+    
+    @State private var rotation = 0.0
+  
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+           Circle()
+                .padding()
+                .rotationEffect(.degrees(rotation))
+                .shadow(color: .black, radius: 3, x: 20, y: 20)
+                .foregroundColor(.orange)
+                .animation(Animation.easeInOut(duration: 2).repeatForever(autoreverses: true))
+                .onReceive(timer){ Input in
+                    yShadow -= -20
+                    
+                    rotation += 300
+                    
+                    
+                                 
+                }
+            ZStack{
+                Text("Jerry Dai")
+                    .padding()
+                    .rotationEffect(.degrees(rotation))
+                    .shadow(color: .black, radius: 3, x: 20, y: 20)
+                    .foregroundColor(.red)
+                    .animation(Animation.easeInOut(duration: 2).repeatForever(autoreverses: true))
+                    .onReceive(timer){ Input in
+                        yShadow -= -20
+                        
+                        rotation += 300
+                        
+                        
+                                     
+                    }
+            }
+            
+            
+//            Circle()
+//                .frame(width: 50, height: 50)
+//
+//
+//
+//            Text("OK")
+//                .foregroundColor(.white)
+//
+//        }
+//        .offset(x: xOffset, y: 0)
+//        .rotationEffect(.degrees(rotation), anchor: .center)
+//        .animation(Animation.easeInOut(duration: 2).repeatForever(autoreverses: true))
+//
+//        .onReceive(timer){ input in
+//            //move the circle and text to the right
+//            xOffset = 100.0
+//
+//            //Turn once
+//            rotation = 360.0
+//
+//            //turn off the time
+//            timer.upstream.connect().cancel()
+        }
     }
 }
 
-struct CustomComposableView_Previews: PreviewProvider {
-    static var previews: some View {
-        CustomComposableView()
-    }
-}
+//struct CustomComposableView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CustomComposableView()
+//    }
+//}
+
--- ComposableViewsAndAnimations/Views/Exercises/ExerciseOneView.swift
+++ ComposableViewsAndAnimations/Views/Exercises/ExerciseOneView.swift
@@ -14,9 +14,18 @@ struct ExerciseOneView: View {
     
     // Controls whether this view is showing or not
     @Binding var showThisView: Bool
+    
+    @State private var scaleFactor: CGFloat = 1.0
         
     // Whether to apply the animation
     @State private var useAnimation = false
+    
+    @State var randColor = Color.blue
+    
+    @State private var xOffset: CGFloat = 0.0
+    
+    @State private var yOffset: CGFloat = 0.0
+    
 
     // MARK: Computed properties
 
@@ -28,7 +37,27 @@ struct ExerciseOneView: View {
 
                 Circle()
                     .frame(width: 200, height: 200)
-                    .foregroundColor(.blue)
+                    .foregroundColor(self.randColor)
+                    .offset(x: xOffset, y: yOffset)
+                    .scaleEffect(scaleFactor)
+                    .onTapGesture {
+                        if scaleFactor > 0.2 {
+                            // Reduce the size of the circle by a tenth
+                            scaleFactor -= 0.1
+                            self.newColor()
+                            
+                        xOffset = CGFloat.random(in: -100...100)
+                        yOffset = CGFloat.random(in: -100...100)
+                        } else {
+                            // Make sure the button doesn't entirely disappear
+                            scaleFactor = 1
+                        }
+                        
+                            
+                          
+                    
+                       
+                    }
                 
             }
             .navigationTitle("Exercise 1")
@@ -50,6 +79,27 @@ struct ExerciseOneView: View {
     func hideView() {
         showThisView = false
     }
+    
+    func newColor() {
+        
+        let rand1 = Double.random(in: 0...255)
+        let rand2 = Double.random(in: 0...255)
+        let rand3 = Double.random(in: 0...255)
+        
+        let randColor = Color(red: rand1/255, green: rand2/255, blue: rand3/255)
+        
+        self.randColor = randColor
+    }
+    
+//    func newLocation() {
+//        let randp1 = Double.random(in: 100...220)
+//        let randp2 = Double.random(in: 100...357)
+//
+//        var randPosition = position(x: randp1, y: randp2)
+//
+//        self.randPosition = randPosition
+//
+//    }
     
 }
 
